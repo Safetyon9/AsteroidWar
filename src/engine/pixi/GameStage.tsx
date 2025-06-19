24b2d89079840f18c30715c0ca1f4d6cacbffe3c
safetyon9 <safetyon99@gmaiil.com>
2025-06-19 19:34:01 +0200

    rotazione su evento

--- src/engine/pixi/GameStage.tsx
+++ src/engine/pixi/GameStage.tsx
@@ -20,6 +20,7 @@ export async function playgroundPixi(containerElement: HTMLDivElement): Promise<
 
     const bunny = new Sprite(texture);
     container.addChild(bunny);
+    bunny.scale.set(4,4);
 
     container.x = app.screen.width / 2;
     container.y = app.screen.height / 2;
@@ -27,8 +28,26 @@ export async function playgroundPixi(containerElement: HTMLDivElement): Promise<
     container.pivot.x = container.width / 2;
     container.pivot.y = container.height / 2;
 
+    bunny.interactive = true;
+
+    bunny.on('pointerover', () => {
+        app.view.style.cursor = 'pointer';
+    });
+
+    bunny.on('pointerout', () => {
+        app.view.style.cursor = 'default';
+    });
+
+    let isRotating = false;
+
+    bunny.on('pointerdown', () => {
+        isRotating = !isRotating;
+    });
+
     app.ticker.add((time) => {
-        container.rotation -= 0.01 * time.deltaTime;
+        if(isRotating) {
+            container.rotation -= 0.05 * time.deltaTime;
+        }
     });
 
     return app;
