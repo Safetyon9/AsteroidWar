d7288373ae58458308bec89756976ed79d216b45
safetyon9 <safetyon99@gmaiil.com>
2025-06-19 16:48:07 +0200

    Creazione del progetto con le tecnologie specificate in specifiche.docx

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+import { createDefaultPreset } from 'ts-jest';
+
+/** @type {import("jest").Config} */
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+export default {
+  testEnvironment: 'node',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
