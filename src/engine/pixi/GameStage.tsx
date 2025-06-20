3cf766a089dd4d1b4c3c7ca0f8f6fae24e588458
safetyon9 <safetyon99@gmaiil.com>
2025-06-20 14:09:39 +0200

    creato il movimento del oggetto giocatore, implementato un tasto pausa.

--- src/engine/pixi/GameStage.tsx
+++ src/engine/pixi/GameStage.tsx
@@ -18,35 +18,75 @@ export async function playgroundPixi(containerElement: HTMLDivElement): Promise<
 
     const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
 
-    const bunny = new Sprite(texture);
-    container.addChild(bunny);
-    bunny.scale.set(4,4);
+    const player = new Sprite(texture);
+    container.addChild(player);
+
+    player.anchor.set(0.5);
+    player.scale.set(4,4);
 
     container.x = app.screen.width / 2;
     container.y = app.screen.height / 2;
 
-    container.pivot.x = container.width / 2;
-    container.pivot.y = container.height / 2;
+    container.pivot.set(0, 0);
+    player.position.set(0, 0);
 
-    bunny.interactive = true;
+    player.interactive = true;
 
-    bunny.on('pointerover', () => {
+    player.on('pointerover', () => {
         app.view.style.cursor = 'pointer';
     });
 
-    bunny.on('pointerout', () => {
+    player.on('pointerout', () => {
         app.view.style.cursor = 'default';
     });
 
     let isRotating = false;
+    let isPause = false;
 
-    bunny.on('pointerdown', () => {
-        isRotating = !isRotating;
+    player.on('pointerdown', () => {
+        if(!isPause){
+            isRotating = !isRotating;
+        }
+    });
+
+    app.stage.eventMode = 'static';
+    app.stage.hitArea = app.screen;
+
+    let targetX = container.x;
+    let targetY = container.y;
+
+    app.stage.on('pointermove', (event) => {
+        const pos = event.global;
+        targetX = pos.x;
+        targetY = pos.y;
+    });
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            if (isPause){
+                isPause = false;
+            } else {
+                isPause = true;
+            } 
+        }
     });
 
     app.ticker.add((time) => {
-        if(isRotating) {
-            container.rotation -= 0.05 * time.deltaTime;
+        if(!isPause) {
+            if(isRotating) {
+                container.rotation -= 0.05 * time.deltaTime;
+            }
+
+            const dx = targetX - container.x;
+            const dy = targetY - container.y;
+            const speed = 0.02;
+
+            const dist = Math.sqrt(dx * dx + dy * dy);
+
+            if (dist > 1) {
+                container.x += dx*speed;
+                container.y += dy*speed;
+            }
         }
     });
 
